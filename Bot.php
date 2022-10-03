04c89d3bd7295452d1e942250fc93851e87e66fc
ExcelentProgrammer <glpat-BUZzgwCbV18KqT7xGML9>
2022-10-03 22:54:08 +0500

    update

--- Bot.php
+++ Bot.php
@@ -3,6 +3,11 @@
 use Telegram\Bot;
 use Database\DB;
 
+Bot::handler(['command'=>"start"],function(){
+    $firstName = Bot::getUser()->first_name;
+    Bot::sendMessage("Assalomu alaykum $firstName Botga hush kelibsiz");
+});
+
 Bot::handler(['text'=>"salom"],function(){
     Bot::sendMessage("salom");
 });
