c19ae6c58872e468059b96e56a16d35c3e238a98
ExcelentProgrammer <glpat-BUZzgwCbV18KqT7xGML9>
2022-10-03 23:11:33 +0500

    update users.php

--- Helpers/Users.php
+++ Helpers/Users.php
@@ -6,5 +6,8 @@ use Telegram\Bot;
 $text = Bot::getText();
 if($text == "/start"){
     $user = Bot::getUser();
-    DB::table(Vars::getUsersTable())->insert(['userId'=>Bot::chatId(),"firstName"=>$user->first_name,"lastName"=>$user->last_name,"userName"=>$user->username,"page"=>"home"])->run();
+    $res = DB::table(Vars::getUsersTable())->select()->where(['userId'=>Bot::chatId()])->run()->RowCount;
+    if($res == 0){
+        DB::table(Vars::getUsersTable())->insert(['userId'=>Bot::chatId(),"firstName"=>$user->first_name,"lastName"=>$user->last_name,"userName"=>$user->username,"page"=>"home"])->run();
+    }
 }
